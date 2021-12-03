264b47ca30d2c896263560ea25f83c4401f7b6f0
Kurt Gonzales <83806081+KurtGonzales@users.noreply.github.com>
2021-12-04 00:27:44 +0800

    update

---
+++ chirp/main.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('nav-links');
+const btn = document.getElementById('hamburger');
+
+btn.addEventListener("click", function(){
+    nav.classList.toggle('show-nav');
+})
